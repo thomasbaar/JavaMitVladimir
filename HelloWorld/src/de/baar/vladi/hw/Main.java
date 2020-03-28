a12e702ac3425d2d159f52465ca3ca41ee2115ef
Baar <Baar@F1-NB-C367-01.intern.f1.htw-berlin.de>
2020-03-28 18:12:20 +0100

    hello wowik

--- HelloWorld/src/de/baar/vladi/hw/Main.java
+++ HelloWorld/src/de/baar/vladi/hw/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello, great world!");
 		System.out.println("Hallo Papa ");
+		System.out.println("Hallo Wowik ");
 		
 
 	}
