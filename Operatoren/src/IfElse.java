a468b9382db318822506bbf4cca996dd25638cf7
Vladi <Vladi@VLADI-PC.fritz.box>
2020-04-03 21:54:36 +0200

    If else switch

---
+++ Operatoren/src/IfElse.java
@@ -0,0 +1,21 @@
+
+public class IfElse {
+	public static void main ( String [] text) {
+		boolean checked ;
+		checked = false ;
+		char state ;
+		if (checked)
+			(state = '+') ;
+		else 
+			(state = '-');
+		System.out.println("Status:" + state);
+		checked = true ;
+		if (checked)
+			(state = '+');
+		else 
+			(state = '-');
+		System.out.println("Status:" +state) ;
+		
+	}
+
+}
---
+++ Operatoren/src/exampleclass/LokaleKlasse.java
@@ -0,0 +1,13 @@
+package exampleclass;
+import rectangle.Rectangle;
+public class LokaleKlasse {
+	private int height ; 
+	private int width;
+	public void Rectangle() {
+		new Pattern();
+	}
+	class Pattern {
+		private int dummy;
+	}
+
+}
