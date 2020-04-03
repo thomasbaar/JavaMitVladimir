a5904ed642daf1d84c306496128b9e289d21f786
Vladi <Vladi@VLADI-PC.mshome.net>
2020-04-03 22:13:59 +0200

    case switch und while schleife

---
+++ Operatoren/src/Caseswitch.java
@@ -0,0 +1,15 @@
+
+public class Caseswitch {
+public static void main(String [] text) {
+	int checked = 0;
+	char state  ;
+	switch (checked) {
+	case 0: state = '-'; 
+	break;
+	case 1: state = '+';
+	break;
+	
+	}
+
+}
+}
---
+++ Operatoren/src/schleifen/WhileSchleife.java
@@ -0,0 +1,15 @@
+package schleifen;
+
+public class WhileSchleife {
+public static void main (String [] text) {
+	byte lange, weile ; 
+	lange = 1;
+	weile = 7; 
+	while (lange < weile) {
+		lange ++; 
+		System.out.println("lange");
+	}
+		System.out.print("langeweile");
+	
+}
+}
